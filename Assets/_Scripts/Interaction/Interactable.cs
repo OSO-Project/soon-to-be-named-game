8d64c5b154b15db365657afcdb449194923d479a
Krystian S. <61630568+Veletor@users.noreply.github.com>
2024-05-23 17:55:41 +0200

    Created Interactable Interface
    Which simply provides three methods and provides a way to refer to interactable objects in code

---
+++ Assets/_Scripts/Interaction/Interactable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem
+
+public interface Interactable
+{
+   //this interface simply provides three methods that interactable objects need to implement
+	void OnBeginLooking();
+	void OnFinishLooking();
+	void OnPressInteract(InputAction.CallbackContext ctx);
+
+}
