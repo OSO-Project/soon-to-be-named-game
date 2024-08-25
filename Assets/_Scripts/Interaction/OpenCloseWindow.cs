e355b2e55082cf9127e4e5115f5bf6f2cece2c3b
Denya56 <56lkj546@gmail.com>
2024-08-25 20:56:20 +0200

    Squashed commit of the following:
    commit defc0119fda0edb08ea88c57c7b52ed04776c5ba
    Author: Maciej Zamojski <maciejzamojski.mz@gmail.com>
    Date:   Sun Aug 25 20:33:40 2024 +0200
    
        walls and tiles prefabs updated. particle system updated, player controller updated

---
+++ Assets/_Scripts/Interaction/OpenCloseWindow.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class OpenCloseWindow : MonoBehaviour, Interactable
+{
+    private bool isUnlocked;
+    private string state;
+    public bool isOpen;
+    private Animator windowAnimator;
+    private OpenCloseWindow currentTarget = null;
+    private MoveWindowHandle windowHandleStatus = null;
+    private void Start()
+    {
+        windowAnimator = GetComponent<Animator>();
+        if (windowAnimator == null)
+        {
+            Debug.LogError("Animator component missing from drawer object.");
+        }
+        
+        windowHandleStatus = transform.Find("HandleHinge").GetComponent<MoveWindowHandle>();
+        if (windowHandleStatus == null)
+        {
+            Debug.LogError("No MoveWindowHandle script attached to the handle.");
+        }
+
+        InputManager.Instance.OpenCloseAction.performed += OnPressInteract;
+    }
+
+    private void OnDestroy()
+    {
+        InputManager.Instance.OpenCloseAction.performed -= OnPressInteract;
+    }
+
+    public void OnBeginLooking()
+    {
+        currentTarget = this;
+        //Code to execute when you aim at the object
+    }
+    public void OnFinishLooking()
+    {
+        currentTarget = null;
+	    //Code to execute when you aim away from the object
+    }
+
+    public void OnPressInteract(InputAction.CallbackContext ctx)
+    {
+        isUnlocked = windowHandleStatus.isUnlocked;
+        state = windowHandleStatus.state;
+
+        if (currentTarget == this)
+        {
+            if (!isUnlocked)
+            {
+                return;
+            }
+            else
+            {
+                if (windowAnimator == null)
+                {
+                    return;
+                }
+
+                if (!isOpen)
+                {
+                    if (state == "Side")
+                    {
+                        windowAnimator.SetTrigger("OpenHorizontal");
+                    }
+                    else if (state == "Up")
+                    {
+                        windowAnimator.SetTrigger("OpenVertical");
+                    }
+                }
+                else
+                {
+                    if (state == "Side")
+                    {
+                        windowAnimator.SetTrigger("CloseHorizontal");
+                    }
+                    else if (state == "Up")
+                    {
+                        windowAnimator.SetTrigger("CloseVertical");
+                    }
+                }
+                isOpen = !isOpen;
+            }
+        }
+    }
+}
